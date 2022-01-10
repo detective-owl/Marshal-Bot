1b9e2d7c71efb8e74815c76b3b9e0c4b52b91bcc
detective-owl <97421454+detective-owl@users.noreply.github.com>
2022-01-10 11:44:17 +0800

    detective-owl modified marshall bot code
    Sharp IR sensor values issue and adding of NRF24L01 integration

--- Marshal_Final/Marshal_Final.ino
+++ Marshal_Final/Marshal_Final.ino
@@ -1,80 +1,121 @@
+#include <SharpIR.h>
+#define SENSOR_SAMPLES 20
+#include <LiquidCrystal_I2C.h>
 #include <Servo.h>
-#include <LiquidCrystal.h>
+//#define ADJUSTMENT +10                     //Compensation for reading variance in cm
 
-LiquidCrystal lcd(13,12,5,4,3,2);
-Servo myServoL,myServoLT,myServoR,myServoRT; // Servo 1
+LiquidCrystal_I2C lcd(0x27,16,2);
+Servo servoLeftTop,servoLeft,servoRightTop,servoRight; //Servo Declarations
+int pos1 = 0, pos2 = 0, pos3 = 0, pos4 =0;   //Servo motor positions
+float dL,dR,dC;                              //Sharp Distance Sensor
+int SensorPin = 0;
 
-int pos1 = 0, pos2 = 0, pos3 = 0, pos4 =0;              // Servo motor positions.
-float distValL, distValR, distValC;                     // Read values of distance sensors.
-int SensorPin = 0;                                      // Analog Distance Sensor
+//The sensor output pin is attached to the pin A0
+SharpIR watchL( SharpIR::GP2Y0A02YK0F, A0 );
+SharpIR watchC( SharpIR::GP2Y0A02YK0F, A1 );
+SharpIR watchR( SharpIR::GP2Y0A02YK0F, A2 );
 
-void setup() {
-  myServoL.attach(11);                             //
-  myServoLT.attach(10);                            // 
-  myServoR.attach(9);                              //   
-  myServoRT.attach(8);                             // 
-  lcd.print("Marshal Bot");                        // LCD Line 1 Name
-  lcd.setCursor(0,1);
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup()
+{
+  servoLeftTop.attach(5);                                 //Left Top Servo
+  servoLeft.attach(4);                                    //Left Servo 
+  servoRightTop.attach(3);                                //Right Top Servo   
+  servoRight.attach(2);                                   //Right Servo
+    
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Marshall Bot");
+  Serial.begin( 9600 ); //Enable the serial comunication
+  pinMode(LED_BUILTIN,OUTPUT);
+ }
+
+char buffer[80] = { 0 };
+
+float getMeanDistance( SharpIR &sensor )               // Scan runway & report distances seen by the 3 sensors:
+{
+  float total = 0;
+  for( int i = 0;  i < SENSOR_SAMPLES;  ++i )
+  {
+    total += sensor.getDistance();
+  }
+  return total / SENSOR_SAMPLES;
 }
 
-void loop() {
-  distValL = convertCM(analogRead(A0));
-  distValC = convertCM(analogRead(A1));
-  distValR = convertCM(analogRead(A2));
-  delay(100);
-  Serial.println(distValL);
-  Serial.println(distValC);
-  Serial.println(distValR);
-  //lcd.print(valToCm);
-  //lcd.print("  cm                ");   
-        if (distValL>= 00 &&distValL<= 12 &&distValC>= 10 &&distValC<= 12 &&distValR>= 10 &&distValR<= 12){
-      // CASE  "Face Me"
-          LCD("Face me             ");
-          MOTION(0,0,0,0);
-    }         
-        if (distValL>= 10 &&distValL<= 12 &&distValC>= 10 &&distValC<= 12 &&distValR>= 10 &&distValR<= 12){
-      // CASE  "Turn Left"
-          LCD("Turn left           ");
-          MOTION(0,0,0,0);
-    }          
-        if (distValL>= 10 &&distValL<= 12 &&distValC>= 10 &&distValC<= 12 &&distValR>= 10 &&distValR<= 12){
-      // CASE  "Turn Right"
-          LCD("Turn right          ");
-          MOTION(0,0,0,0);
-    }              
-        if (distValL>= 10 &&distValL<= 12 &&distValC>= 10 &&distValC<= 12 &&distValR>= 10 &&distValR<= 12){
-      // CASE  "Slow Down"
-          LCD("Slow down           ");
-          MOTION(0,0,0,0);
-    }                
-        if (distValL>= 10 &&distValL<= 12 &&distValC>= 10 &&distValC<= 12 &&distValR>= 10 &&distValR<= 12){
-      // CASE  "Proceed Forward"
-          LCD("Proceed Forward     ");
-          MOTION(0,0,0,0);
-    }          
-        if (distValL>= 10 &&distValL<= 12 &&distValC>= 10 &&distValC<= 12 &&distValR>= 10 &&distValR<= 12){
-      // CASE  "Stop"
-          LCD("Stop                ");
-          MOTION(0,0,0,0);
-    }                 
+void loop()
+{
+  int dL = getMeanDistance( watchL );
+  int dC = getMeanDistance( watchC );
+  int dR = getMeanDistance( watchR );
+  snprintf( buffer, 80, "L:[%2d] C:[%2d] R:[%2d]\n", dL, dC, dR );
+  Serial.println( buffer );
+//  show(buffer);
+    
+  if( dL >= 77 && dL <= 93 && dC >= 65 && dC <= 132 && dR >= 45 && dR <= 55 )
+  {
+    show("Face me ");
+    // waiting for an aircrraft <face me>;
+    moveArms(60,0,60,0);
+    
+  }
+    
+  else if( dL >= 114 && dL <= 115 && dC >= 30 && dC <= 60 && dR >= 51 && dR <= 52 )
+  {
+    show("Go ahead ");
+    // straight & in the middle of the runway <go straight>;
+    moveArms(90,0,0,0); 
+  }
+      
+  else if( dL >= 58 && dL <= 60 && dC >= 32 && dC <= 66 && dR >= 26 && dR <= 38 )
+  {
+    show("Go Left ");
+    // on the right side of the runway <go left>;
+    moveArms(130,0,130,0);
+    
+  }
+  else if( dL >= 26 && dL <= 36 && dC >= 36 && dC <= 58 && dR >= 51 && dR <= 65 )
+  {
+    show("Go Right ");
+    // on the left side of the runway <go right>;
+    moveArms(90,0,60,0);
+  }
+    
+  else if( dL >= 108 && dL <= 115 && dC >= 25 && dC <= 28 && dR >= 42 && dR <= 49 )
+  {
+    show("Slow Down ");
+    // approaching to a stop <slow down>;
+    moveArms(90,0,0,0);
+    
+  }
+  else if( dL >= 114 && dL <= 118 && dC >= 19 && dC <= 22 && dR >= 49 && dR <= 45 )
+  {
+    show("Stop ");
+    // stoping an aircraft <stop>;
+    
+  }
+//    else
+//  {
+//    show( buffer );
+//  }
+  
 }
 
-// Conversion from distance sensor to CM
-float convertCM(float readValue){
-  return 12343.85*pow(readValue,-1.15);
-}
 // 4 Servo Motor motion control
-void MOTION(int L,int LT, int R, int RT){
-  myServoL.write(L);
-  myServoLT.write(LT);
-  myServoR.write(R);
-  myServoRT.write(RT);
+void moveArms( int leftTop,int left,int rightTop,int right)
+{
+  servoLeftTop.write( leftTop );
+  servoLeft.write( left );
+  servoRightTop.write( rightTop );
+  servoRight.write( right );
+  
 }
-// LCD Display
-void LCD(String S) {
+
+//LCD Display
+void show(String S) {
   lcd.setCursor(0,1);
   lcd.print(S);
 }
 
+//  snprintf( buffer, 80, "L: [%3d] C: [%3d] cR: [%3d]\n", dL, dC, dR );
+//  Serial.println( buffer );
+//}
